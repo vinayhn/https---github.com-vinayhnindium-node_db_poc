13b932ca3aa884376ec4f007acd88edb869434d2
vinayhnindium <vinay.hatyal@indiumsoft.com>
2022-02-04 16:13:37 +0530

    split_table_commit

--- controller/getDeviceMatrix.js
+++ controller/getDeviceMatrix.js
@@ -7,7 +7,7 @@ const getDevices = async (req, res) => {
   const {
     userId,
   } = req.query;
-  const sql = 'SELECT * FROM users.device FULL JOIN users."matrixInfo" ON  users.device.device_id = users."matrixInfo".device_id WHERE user_id =  $1;';
+  const sql = 'SELECT UD.device_id,device_name,app_name,CU.cpu_app_usage,GU.avg_gpu_usage,MU.avg_memory_usage,PU.avg_power_usage FROM users.device UD FULL JOIN users.cpu_usage CU ON  UD.device_id = CU.device_id FULL JOIN users.gpu_usage GU ON  CU.device_id = GU.device_id FULL JOIN users.memory_usage MU ON  GU.device_id = MU.device_id FULL JOIN users.power_usage PU ON  MU.device_id = PU.device_id WHERE UD.user_id = $1';
   pool.query(sql, [userId], (error, results) => {
     if (error) {
       throw error;
@@ -20,7 +20,7 @@ const getDevice = async (req, res) => {
   const {
     userId, deviceId,
   } = req.query;
-  const sql = 'SELECT * FROM users.device FULL JOIN users."matrixInfo" ON  users.device.device_id = users."matrixInfo".device_id WHERE user_id =  $1 AND device.device_id = $2;';
+  const sql = 'SELECT UD.device_id,device_name,app_name,CU.cpu_app_usage,GU.avg_gpu_usage,MU.avg_memory_usage,PU.avg_power_usage FROM users.device UD FULL JOIN users.cpu_usage CU ON  UD.device_id = CU.device_id FULL JOIN users.gpu_usage GU ON  CU.device_id = GU.device_id FULL JOIN users.memory_usage MU ON  GU.device_id = MU.device_id FULL JOIN users.power_usage PU ON  MU.device_id = PU.device_id WHERE UD.user_id = $1  AND UD.device_id = $2;';
   pool.query(sql, [userId, deviceId], (error, results) => {
     if (error) {
       throw error;
--- controller/matrixInfo.js
+++ controller/matrixInfo.js
@@ -7,8 +7,14 @@ const addMatrix = async (req, res) => {
   const {
     cpuAppUsage, avgMemoryUsage, avgPowerUsage, avgGpuUsage,
   } = req.body;
-  const sql = 'INSERT INTO users."matrixInfo"(session_id, cpu_app_usage, avg_memory_usage, avg_power_usage, avg_gpu_usage, device_id) VALUES ($1,$2,$3,$4,$5,$6)';
-  pool.query(sql, [req.session.sessionID, cpuAppUsage, avgMemoryUsage, avgPowerUsage, avgGpuUsage, req.session.deviceId]);
+  const cpuSql = 'INSERT INTO users."cpu_usage"(session_id, cpu_app_usage, device_id) VALUES ($1,$2,$3)';
+  const memoryUsageSql = 'INSERT INTO users."memory_usage"(session_id,avg_memory_usage,device_id) VALUES ($1,$2,$3)';
+  const powerUsageSql = 'INSERT INTO users."power_usage"(session_id,avg_power_usage,device_id) VALUES ($1,$2,$3)';
+  const gpuUsageSql = 'INSERT INTO users."gpu_usage"(session_id,avg_gpu_usage,device_id) VALUES ($1,$2,$3)';
+  pool.query(cpuSql, [req.session.sessionID, cpuAppUsage, req.session.deviceId]);
+  pool.query(memoryUsageSql, [req.session.sessionID, avgMemoryUsage, req.session.deviceId]);
+  pool.query(powerUsageSql, [req.session.sessionID, avgPowerUsage, req.session.deviceId]);
+  pool.query(gpuUsageSql, [req.session.sessionID, avgGpuUsage, req.session.deviceId]);
   return res.status(200).json({
     message: 'new device matrix info added',
   });
